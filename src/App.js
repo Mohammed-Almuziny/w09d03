ad4ea9e29d0185fb754a02c4ee537cbdf1b602b5
Mohammed-Almuziny <mohammed.abdullah.js@tuwaiq.edu.sa>
2021-12-07 19:09:44 +0300

    create register page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { Routes, Route } from "react-router";
+import dotenv from "dotenv";
+
 import { Header } from "./components/Header";
+import { Register } from "./pages/Register";
+
+dotenv.config();
 
 function App() {
   return (
     <div>
       <Header />
-      app
+
+      <Routes>
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </div>
   );
 }
